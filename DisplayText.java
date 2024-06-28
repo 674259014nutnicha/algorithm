9cc60749f70ac45cdabc96ed377fbe6136ce0ce3
674259014nutnicha <674259014@webmail.npru.ac.th>
2024-06-28 12:04:38 +0700

    first commit

---
+++ DisplayText.java
@@ -0,0 +1,10 @@
+public class DisplayText {
+
+    public static void main(String[] args) {
+        System.out.println("Hollo World");
+        System.out.println("Nutnicha " + "Khaminsom");
+        System.out.println(10 + 5);
+        System.out.println("NPRU " + 2567);
+        System.out.println(0+ "3" + 2);
+    }
+}
---
+++ HelloWord2.java
@@ -0,0 +1,9 @@
+public class HelloWord2 {
+    public static void main(String[] args) {
+        System.out.println("Hollo World");
+        System.out.println("Nutnicha " + "Khaminsom");
+        System.out.println(10 + 5);
+        System.out.println("NPRU " + 2567);
+        System.out.println(0+ "3" + 2);
+    }
+}
